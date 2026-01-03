da34596fdcfdef031160d9597dca50295a242462
haniframadhani <69223521+haniframadhani@users.noreply.github.com>
2026-01-03 17:08:22 +0700

    feat: add about section

--- app/components/home/About.vue
+++ app/components/home/About.vue
@@ -1,7 +1,23 @@
 <template>
   <section class="my-16">
-    <h2 class="font-display font-bold text-3xl md:text-4xl">
+    <h2 class="font-display font-bold text-3xl md:text-4xl mb-12">
       Halo, Saya Hanif
     </h2>
+    <div class="flex flex-col space-y-4 text-base">
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet neque
+        fuga ipsum facilis reiciendis. Quisquam, iste consectetur ducimus est,
+        tempora soluta eaque quo perferendis quidem mollitia tenetur illum sint.
+        Esse, natus omnis! Asperiores tempore alias molestiae minus recusandae,
+        mollitia voluptatum eveniet reiciendis non expedita quam officiis,
+        cupiditate laudantium odit voluptates.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga itaque
+        eaque molestias, perspiciatis consequatur, nihil reprehenderit, iusto
+        exercitationem odit perferendis officia dignissimos amet unde delectus
+        quaerat ab placeat nesciunt ducimus?
+      </p>
+    </div>
   </section>
 </template>
