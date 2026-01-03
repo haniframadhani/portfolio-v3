06dfad7d1811688249d13d298615a5500c666c02
haniframadhani <69223521+haniframadhani@users.noreply.github.com>
2026-01-03 11:53:28 +0700

    feat: add blog cards component

---
+++ app/components/card/Blog.vue
@@ -0,0 +1,21 @@
+<template>
+  <UCard
+    variant="soft"
+    class="hover:shadow-lg hover:shadow-secondary-800 duration-300"
+  >
+    <div class="grid grid-cols-1 gap-8">
+      <NuxtImg
+        src="/images/blog.jpg"
+        class="size-full aspect-4/3 object-cover rounded-md overflow-hidden"
+      />
+      <div class="grid gap-2">
+        <h3 class="font-display font-bold text-3xl">Title of Blog</h3>
+        <p class="line-clamp-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+          quaerat accusantium autem animi molestiae temporibus at consequatur
+          numquam blanditiis voluptate?
+        </p>
+      </div>
+    </div>
+  </UCard>
+</template>
--- app/components/home/Blog.vue
+++ app/components/home/Blog.vue
@@ -1,7 +1,16 @@
+<script setup lang="ts">
+import Blog from "../card/Blog.vue";
+</script>
+
 <template>
   <section class="my-16">
-    <h2 class="font-display font-bold text-3xl md:text-4xl">
+    <h2 class="font-display font-bold text-3xl md:text-4xl mb-12">
       Some of my insight
     </h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+      <Blog />
+      <Blog />
+    </div>
+    <NuxtLink to="#" class="block text-center underline">More of my insight</NuxtLink>
   </section>
 </template>
